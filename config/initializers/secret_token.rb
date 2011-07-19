7315a681cad3dbbd6b28c2f6b7bf45c3155b0a97
cocaineteddybear <alexander.didenko@gmail.com>
2011-07-19 17:21:11 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nphbf::Application.config.secret_token = '7c8a3fb68c648d7ef8bc0ac640c66502f2dd4aeecb330f3102394d1117be08f7bbd809e04c26ef7b4937a48b45d77b98a23b3cbb9a8a504a74f41dff42c3d000'
